a49e1b6caed4bd6ec3add74f399be8e7c6551acc
pengyangyan <531639577@qq.com>
2019-05-07 16:24:45 +0800

    bug

--- src/main/java/com/cqjtu/studentdocument/advice/ExceptionEnums.java
+++ src/main/java/com/cqjtu/studentdocument/advice/ExceptionEnums.java
@@ -14,11 +14,11 @@ public enum ExceptionEnums {
     /**
      * 错误返回信息
      */
-    NO_CHECK_INFO(404,"暂无健康档案表，无法进行数据分析"),
-    CHOOSE_FILE(404,"文件未上传"),
-    IS_NOT_LOGIN(404,"用户未登录"),
-    UPLOAD_FAIL(404,"上传失败"),
-    RESOURCE_FOUNT_FAIL(404,"获取菜单权限失败！"),
+    NO_CHECK_INFO(400,"暂无健康档案表，无法进行数据分析"),
+    CHOOSE_FILE(400,"文件未上传"),
+    IS_NOT_LOGIN(400,"用户未登录"),
+    UPLOAD_FAIL(400,"上传失败"),
+    RESOURCE_FOUNT_FAIL(400,"获取菜单权限失败！"),
     NOT_AUTHORIZED(403,"您暂无权限访问该资源！"),
     ACCOUNT_IS_EXIT(400,"用户名已存在"),
     PASSWORD_WRONG(400,"密码错误"),
